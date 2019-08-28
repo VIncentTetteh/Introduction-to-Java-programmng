aeb7f99c5fbb897136fb47d0f7a09da0707b1aac
chrisbone <chrisbone@localhost.localdomain>
2019-08-28 17:57:58 +0000

    just trying

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+    public static void main(String[] args)
+    {
+       String name = "vincent Tetteh";
+       System.out.println(name.replace('v', 'c'));
+       
+       
+    }
+}
